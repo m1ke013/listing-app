4cd0fdb2a574dda2bc1e1134edfed31e95e32690
unknown <Mohammad Michael Mip@JZ28B-239.JZ28B-239>
2023-05-10 15:22:54 +0800

    UI and Event for Insert

--- resources/views/products/add_product.blade.php
+++ resources/views/products/add_product.blade.php
@@ -1,21 +1,23 @@
 @include('layout.cdn')
-@include('layout.header.header')
+@include('layout.header.navigation')
 
 <div class="container">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <form>
+        <form action="{{route('products.insertProduct')}}" enctype="multipart/form-data" >
+        @method('POST')
+        @csrf
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" name="name">
             </div>
 
             <div class="form-group">
                 <label for="details">Details</label>
-                <textarea class="form-control" id="details" rows="3"></textarea>
+                <textarea class="form-control" id="details" rows="3" name="details"></textarea>
             </div>
             <div class="form-group">
                 <label for="category">Category</label>
-                <select class="form-control" id="category">
+                <select class="form-control" id="category" name="category">
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -25,12 +27,13 @@
             </div>
             <div class="form-group">
                 <label for="quantity">quantity</label>
-                <input type="number" class="form-control">
+                <input type="number" class="form-control" name="quantity">
             </div>
             <div class="form-group">
                 <label for="price">price</label>
-                <input type="number" class="form-control">
+                <input type="number" class="form-control" name="price">
             </div>
+            <button class="btn btn-success">Submit</button>
         </form>
     </div>
 </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,19 +1,23 @@
 @include('layout.cdn')
-
+@include('layout.header.navigation')
 
 <!-- for debug -->
 <!-- <p>{{$products}}</p> -->
+<!-- <h1>TEST</h1> -->
 
 <div class="container-fluid ">
-<h1>TEST</h1>
-    @if (count($products) > 0)
+
+@if (count($products) > 0)
         @foreach ($products as $product)
         <div class="row">
             <div class="col-sm-5">
-                <div class="card">
+                <div class="card" style="width: 18rem; padding: 5">
                 <div class="card-body">
                     <h5 class="card-title">{{ $product->name }}</h5>
-                    <p class="card-text">{{$product->description}}</p>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{$product->description}}</h6>
+                    <p class="card-text">{{$product->price}}</p>
+                    <a href="#" class="btn btn-primary">edit</a>
+                    <a href="#" class="btn btn-danger">delete</a>
                 </div>
                 </div>
             </div>
